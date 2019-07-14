ddcd8d3380ee26d8a58e7bc288d5ae0e964729a1
takabay <48586083+takabay@users.noreply.github.com>
2019-07-14 13:10:11 -0500

    Add in multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,13 @@
 public class Calculator {
-    public static double add(double num1,double num2){
+    public static double add(double num1, double num2) {
         return num1 + num2;
     }
-    public static double subtract(double num1,double num2){
+
+    public static double subtract(double num1, double num2) {
         return num1 - num2;
     }
-    public static double multiply(double num1, double num2){
-        return num*num2;
+
+    public static double multiply(double num1, double num2) {
+        return num2 * num2;
+    }
 }
